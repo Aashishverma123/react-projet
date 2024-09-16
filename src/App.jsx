f881eca2fe7ff29cfa0b53587d988b2e6974f612
aashishpatel0789@gmail.com <aashishpatel0789@gmail.com>
2024-09-16 13:27:59 +0530

    changes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 const APP = function(){
     return(
-      <div>
-        <h1>
-          hello my new project
-        </h1>
-      </div>
+      <div className="mt-5 w-[80%] m-auto">
+      <h1 className="text-5xl">This is React Vite Boileapate</h1>
+      <h2 className="text-3xl">{import.meta.env.VITE_API_KEY}</h2>
+      <button className="px-5 py-2 bg-red-300 rounded ">Explore</button>
+    </div>
     )
 }
 
